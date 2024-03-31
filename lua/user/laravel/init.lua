25b0d4873a1d25379d1cbc8180b212ed9f885be5
Bryant Dinh <bryantcs120@gmail.com>
2024-03-31 11:52:50 -0700

    Cleaning up commented out setup for laravel plugin.

--- lua/user/laravel/init.lua
+++ lua/user/laravel/init.lua
@@ -9,29 +9,5 @@ table.insert(lvim.plugins,
     },
     cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
     event = { "VeryLazy" },
-    -- config = function()
-    --   -- Laravel LSP
-    --   require("laravel").setup({ lsp_server = "intelephense" })
-    --   -- Laravel Telescope
-    --   require("telescope").load_extension("laravel")
-    --   lvim.builtin.which_key.mappings["["] = {
-    --     name = "Laravel",
-    --     a = { "<cmd>Laravel artisan<cr>", "Artisan" },
-    --     r = { "<cmd>Laravel routes<cr>", "Routes" },
-    --     m = { "<cmd>Laravel related<cr>", "Related" },
-    --     T = { "<cmd>TermExec cmd='php artisan test'<cr>", "Test" },
-    --     t = {
-    --       function()
-    --         local file_name = vim.fn.expand("%:t:r")
-    --         vim.api.nvim_command("TermExec cmd='php artisan test --filter " .. file_name .. "'")
-    --       end,
-    --       "Test current file"
-    --     },
-    --     p = {
-    --       function() require("laravel.tinker").send_to_tinker() end,
-    --       "Tinker",
-    --     },
-    --   }
-    -- end,
   }
 )
