c27d87903575b1a6ebdec68f5ae3ef5b0932620a
Bryant Dinh <bryantcs120@gmail.com>
2024-04-02 05:28:08 -0700

    Improved blade highlighting and highlighting for alpine.

--- after/queries/blade/injections.scm
+++ after/queries/blade/injections.scm
@@ -2,3 +2,15 @@
     (#not-has-ancestor? @injection.content "envoy")
     (#set! injection.combined)
     (#set! injection.language php))
+
+((text) @injection.content
+    (#has-ancestor? @injection.content "envoy")
+    (#set! injection.combined)
+    (#set! injection.language bash))
+
+
+((php_only) @injection.content
+    (#set! injection.combined)
+    (#set! injection.language php_only))
+((parameter) @injection.content
+    (#set! injection.language php_only))
