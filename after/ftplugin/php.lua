8e949bff462e76434aa09ae2e5c75e8dbab8df79
Bryant Dinh <bryantcs120@gmail.com>
2024-04-02 03:46:01 -0700

    Looking for next available port now for dap.

--- after/ftplugin/php.lua
+++ after/ftplugin/php.lua
@@ -1,16 +1,22 @@
 local dap = require('dap')
 
 dap.adapters.php = {
-    type = 'executable',
-    command = 'node',
-    args = {'/home/bdinh/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js'}
+  type = 'executable',
+  command = 'node',
+  args = { '/home/bdinh/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js' }
 }
 
 dap.configurations.php = {
-    {
-        type = 'php',
-        request = 'launch',
-        name = 'Listen for Xdebug',
-        port = 9003
-    }
+  {
+    type = 'php',
+    request = 'launch',
+    name = 'Listen for Xdebug',
+    port = function()
+      local port = 9003
+      while vim.fn.system('lsof -i :' .. port) ~= '' do
+        port = port + 1
+      end
+      return port
+    end
+  }
 }
