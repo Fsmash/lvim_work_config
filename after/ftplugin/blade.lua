1a7b3f66f60a677694f84f442fc5d4046846ee06
Bryant Dinh <bryantcs120@gmail.com>
2024-12-13 14:02:12 -0800

    Dap configuration for blade files.

--- after/ftplugin/blade.lua
+++ after/ftplugin/blade.lua
@@ -16,6 +16,28 @@ parser_config.blade = {
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { name = "blade_formatter" }, }
 
+local dap = require('dap')
+dap.adapters.blade = {
+  type = 'executable',
+  command = 'node',
+  args = { '/home/bdinh/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js' }
+}
+
+dap.configurations.blade = {
+  {
+    type = 'blade',
+    request = 'launch',
+    name = 'Listen for Xdebug',
+    port = function()
+      local port = 9003
+      while vim.fn.system('lsof -i :' .. port) ~= '' do
+        port = port + 1
+      end
+      return port
+    end
+  }
+}
+
 -- Comment toggle for Blade TODO: not working
 -- local ft = require("Comment.ft")
 -- ft.blade = '{{--%s--}}'
